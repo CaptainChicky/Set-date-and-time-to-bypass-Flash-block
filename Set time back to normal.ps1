203948751a4fe08a8de3959072c63956f0262170
Captain Chicky <maxfang2015@yahoo.com>
2021-01-12 09:35:14 -0700

    Add files via upload

---
+++ Set time back to normal.ps1
@@ -0,0 +1,14 @@
+<# This will allow this script to run as admin. #>
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
+net start w32time
+w32tm /resync /nowait /force
+net stop w32time
+pause
+
